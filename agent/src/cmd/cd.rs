4f03db637d65c6866d761d8954a880613faf7862
Michael Taggart <michael.t.taggart@gmail.com>
2022-03-05 22:22:02 -0800

    Update cd for arg vecs

--- agent/src/cmd/cd.rs
+++ agent/src/cmd/cd.rs
@@ -4,10 +4,11 @@ use std::env::set_current_dir;
 
 /// Changes the directory using system tools
 /// Rather than the shell
-pub fn handle(s: &String) -> Result<String, Box<dyn Error>> {
-    let new_path = Path::new(s.trim());
+pub fn handle(cmd_args: Vec<String>) -> Result<String, Box<dyn Error>> {
+    let path_arg = cmd_args[0];
+    let new_path = Path::new(&path_arg);
     match set_current_dir(new_path) {
-        Ok(_) => Ok(format!("Changed to {s}").to_string()),
+        Ok(_) => Ok(format!("Changed to {path_arg}").to_string()),
         Err(e) => Ok(e.to_string())
     }
 }
