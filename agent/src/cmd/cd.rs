85423184dde48ee91cd7b8b4056545c26c182b6d
Michael Taggart <michael.t.taggart@gmail.com>
2022-03-05 23:18:00 -0800

    Refactor for CommandArgs

--- agent/src/cmd/cd.rs
+++ agent/src/cmd/cd.rs
@@ -1,11 +1,12 @@
 use std::error::Error;
 use std::path::Path;
 use std::env::set_current_dir;
+use crate::cmd::CommandArgs;
 
 /// Changes the directory using system tools
 /// Rather than the shell
-pub fn handle(cmd_args: Vec<String>) -> Result<String, Box<dyn Error>> {
-    let path_arg = cmd_args[0];
+pub fn handle(cmd_args: CommandArgs) -> Result<String, Box<dyn Error>> {
+    let path_arg = cmd_args.nth(0).unwrap_or_else(|| ".".to_string());
     let new_path = Path::new(&path_arg);
     match set_current_dir(new_path) {
         Ok(_) => Ok(format!("Changed to {path_arg}").to_string()),
