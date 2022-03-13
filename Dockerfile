32078d5c9b2cd69547a518e5cdcfe6ddabf52880
husky <huskyhacks.mk@gmailcom>
2022-03-12 19:40:40 -0500

    dockerfile runs with macos build instruction

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN mkdir /opt/OffensiveNotion
 WORKDIR /opt/OffensiveNotion
 COPY agent/ .
 
-RUN rustup target add x86_64-pc-windows-gnu && rustup toolchain install stable-x86_64-pc-windows-gnu
+RUN rustup target add x86_64-pc-windows-gnu && rustup toolchain install stable-x86_64-pc-windows-gnu && rustup target add x86_64-apple-darwin && rustup install stable-x86_64-apple-darwin
 
 # This Dockerfile gets edited dynamically by main.py. If using main.py, don't touch it. If building the Docker container from source, edit this with your target build and OS
 RUN cargo build {OS} {RELEASE}
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import sys
 
 parser = argparse.ArgumentParser(description='OffensiveNotion Setup. Must be run as root. Generates the '
                                              'OffensiveNotion agent in a container.')
-parser.add_argument('-o', '--os', choices=['linux', 'windows'], help='Target OS')
+parser.add_argument('-o', '--os', choices=['linux', 'windows', 'macos'], help='Target OS')
 parser.add_argument('-b', '--build', choices=['debug', 'release'], help='Binary build')
 parser.add_argument('-c', '--c2lint', default=False, action="store_true", help="C2 linter. Checks your C2 config "
                                                                                "by creating a test page on your "
