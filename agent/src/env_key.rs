76d6894b61945467d265af9ec995d6b261aff929
husky <huskyhacks.mk@gmail.com>
2022-03-27 17:24:34 -0400

    case insensitive username key matching

--- agent/src/env_key.rs
+++ agent/src/env_key.rs
@@ -13,21 +13,40 @@ pub async fn check_env_keys(config_options: &mut ConfigOptions) -> bool {
         return true
     }
 
+    let key_username = config_options.key_username.to_lowercase();
+
     println!("[+] Keying username: {}", config_options.key_username);
 
     // But if there are configs to check against, perform the required checks. If they pass, return from the program and carry on.
 
-    let session_username: String = username();
+    let session_username: String = username().to_lowercase();
 
     println!("[+] Session username: {}", username());
     
     // If the checks fail, kill the program outright.
-    if session_username == config_options.key_username {
+
+    #[cfg(target_os = "linux")] {
+    if session_username == key_username || session_username ==  "root" {
         // true back in the main method continues the program
         return true
     }
+
     else {
         // false back in the main method kills/selfdestructs the program
         return false
     }
+    }
+
+    #[cfg(target_os = "windows")] {
+        if session_username == key_username || session_username ==  "SYSTEM" {
+            // true back in the main method continues the program
+            return true
+        }
+    
+        else {
+            // false back in the main method kills/selfdestructs the program
+            return false
+        }
+        }
+
 }
